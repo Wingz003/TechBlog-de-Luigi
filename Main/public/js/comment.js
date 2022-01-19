923035af2122adb94c935acc1ec30c430ae9e088
Wingz003 <Lsanti618@gmail.com>
2022-01-19 08:35:31 -0600

    initial

--- Main/public/js/comment.js
+++ Main/public/js/comment.js
@@ -1,27 +1,27 @@
 const commentFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // Collect values from the login form
-    
-    const comment = document.querySelector('#comment-text').value.trim();
-    const blogId = event.currentTarget.id;
-    const userName = event.currentTarget.name;
-    if (comment) {
-      // Send a POST request to the API endpoint
-      const response = await fetch('/api/comment', {
-        method: 'POST',
-        body: JSON.stringify({ comment: comment, blog_id: blogId, userName: name }), // find blog id via handlebars or other source
-        headers: { 'Content-Type': 'application/json' },
-      });
-      document.location.replace(`/blog/${blogId}`);
-    }
- 
-  };
+  event.preventDefault();
 
-  try {
-    document
-      .querySelector('.comment-form')
-      .addEventListener('submit', commentFormHandler);
-    } catch {
-    
-    }
+  // Collect values from the login form
+
+  const comment = document.querySelector('#comment-text').value.trim();
+  const blogId = event.currentTarget.id;
+  const userName = event.currentTarget.name;
+  if (comment) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/comment', {
+      method: 'POST',
+      body: JSON.stringify({ comment: comment, blog_id: blogId, userName: name }), // find blog id via handlebars or other source
+      headers: { 'Content-Type': 'application/json' },
+    });
+    document.location.replace(`/blog/${blogId}`);
+  }
+
+};
+
+try {
+  document
+    .querySelector('.comment-form')
+    .addEventListener('submit', commentFormHandler);
+} catch {
+
+}
